b7771117918403aab6ca1930cf095a413b39234e
minahanjlaptop <minahanj@lafayette.edu>
2017-03-24 18:18:31 -0400

    pretty confident that rt.receivepacket is correct?

--- project_2_code/ReceiverTransport.java
+++ project_2_code/ReceiverTransport.java
@@ -22,20 +22,29 @@ public class ReceiverTransport
 
     public void receiveMessage(Packet pkt)
     {
-        ra.receiveMessage(pkt.getMessage());
+        if(!pkt.isCorrupt()) {
+            ra.receiveMessage(pkt.getMessage());
+        }
 
-        if(usingTCP) {
+        if(usingTCP) { //using tcp
 
-            if(pkt.isCorrupt()) {
-                Packet newPacket = new Packet(new Message(""), 0, lastReceivedSeqNum, 0);
-            } else { //send ack
-                Packet newPacket = new Packet(new Message(""), 0, pkt.getSeqnum() + 1, 0);
+            if(pkt.isCorrupt()) { //send a previous ack
+                Packet newPacket = new Packet(new Message("Ack"), 0, lastReceivedSeqNum + 1, 0);
+                nl.sendPacket(newPacket,0);
+            } else { //send appropriate ack
+                Packet newPacket = new Packet(new Message("Ack"), 0, pkt.getSeqnum() + 1, 0);
                 nl.sendPacket(newPacket,0);
                 lastReceivedSeqNum = pkt.getSeqnum();
             }
         } else {//using GBN
-            Packet newPacket = new Packet(new Message(""), 0, pkt.getSeqnum(), 0);
-            nl.sendPacket(newPacket,0);
+            if(pkt.isCorrupt()) {//send a previous ack
+                Packet newPacket = new Packet(new Message("Ack"), 0, lastReceivedSeqNum, 0);
+                nl.sendPacket(newPacket,0);
+            } else { //send appropriate ack
+                Packet newPacket = new Packet(new Message("Ack"), 0, pkt.getSeqnum(), 0);
+                nl.sendPacket(newPacket,0);
+                lastReceivedSeqNum = pkt.getSeqnum();
+            }
         }
     }
 
